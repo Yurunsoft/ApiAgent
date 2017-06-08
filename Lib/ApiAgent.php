156655aa2602295843c3e90de8d5f74f2d013fb1
宇润 <admin@yurunsoft.com>
2017-06-08 16:02:51 +0800

    新增版本号定义

--- Lib/ApiAgent.php
+++ Lib/ApiAgent.php
@@ -3,9 +3,11 @@ namespace Yurun\ApiAgent;
 
 class ApiAgent
 {
+	const VERSION = '0.0.1';
 	public static $config;
 	public static function run($mode)
 	{
+		header('X-Powered-By:ApiAgent ' . VERSION, false);
 		defined('ROOT_PATH') or define('ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 		self::$config = include ROOT_PATH . 'Config/config.php';
 		Plugin::init(self::$config['plugins']);
