cf41d38243d734a1443afcf60853fdbf48dda508
宇润 <admin@yurunsoft.com>
2017-06-09 09:06:32 +0800

    新增两个配置项来支持一些特殊运行环境(如天朝的垃圾虚拟主机们)

--- Config/config.php
+++ Config/config.php
@@ -4,5 +4,9 @@ return array(
 	'plugins'			=>	array(
 		'Cross\\RequestFilter',
 		'Batch\\RequestFilter',
-	)
+	),
+	// 临时目录地址，为空则取系统默认
+	'temp_dir'	=>	'',
+	// 使用自定义实现的重定向，性能较差。如果不是环境不支持自动重定向，请勿设为true
+	'http_custom_location'	=>	false,
 );
--- Lib/ApiAgent.php
+++ Lib/ApiAgent.php
@@ -1,15 +1,25 @@
 <?php
 namespace Yurun\ApiAgent;
 
+use Yurun\Until\HttpRequest;
+
 class ApiAgent
 {
 	const VERSION = '0.0.1';
 	public static $config;
 	public static function run($mode)
 	{
-		header('X-Powered-By:ApiAgent ' . VERSION, false);
+		header('X-Powered-By:ApiAgent ' . self::VERSION, false);
 		defined('ROOT_PATH') or define('ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 		self::$config = include ROOT_PATH . 'Config/config.php';
+		if(isset(self::$config['temp_dir'][0]))
+		{
+			HttpRequest::$tempDir = self::$config['temp_dir'];
+		}
+		if(self::$config['http_custom_location'])
+		{
+			HttpRequest::$customLocation = true;
+		}
 		Plugin::init(self::$config['plugins']);
 		$className = 'Yurun\\ApiAgent\\Mode\\' . $mode;
 		$obj = new $className;
